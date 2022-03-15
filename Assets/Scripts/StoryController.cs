2af859a753790ad843be9657eb5ab48863ba6f53
masterxolos <ozgur-2000-6@hotmail.com>
2022-03-15 12:31:51 +0300

    .

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -36,19 +36,27 @@ public class StoryController : MonoBehaviour
     {
         texts[4].SetActive(false);
         texts[5].SetActive(true);
+        texts[6].SetActive(true);
+        texts[7].SetActive(true);
     }
     public void onButton5Pressed()
     {
         texts[5].SetActive(false);
-        texts[6].SetActive(true);
+        texts[6].SetActive(false);
+        texts[7].SetActive(false);
+        texts[8].SetActive(true);
     }
     public void onButton6Pressed()
     {
+        texts[5].SetActive(false);
         texts[6].SetActive(false);
-        texts[7].SetActive(true);
+        texts[7].SetActive(false);
+        texts[8].SetActive(true);
     }
     public void onButton7Pressed()
     {
+        texts[5].SetActive(false);
+        texts[6].SetActive(false);
         texts[7].SetActive(false);
         texts[8].SetActive(true);
     }
@@ -56,33 +64,50 @@ public class StoryController : MonoBehaviour
     {
         texts[8].SetActive(false);
         texts[9].SetActive(true);
+        texts[10].SetActive(true);
+        texts[11].SetActive(true);
     }
     public void onButton9Pressed()
     {
         texts[9].SetActive(false);
-        texts[10].SetActive(true);
+        texts[10].SetActive(false);
+        texts[11].SetActive(false);
+        texts[12].SetActive(true);
     }
     public void onButton10Pressed()
     {
+        texts[9].SetActive(false);
         texts[10].SetActive(false);
-        texts[11].SetActive(true);
+        texts[11].SetActive(false);
+        texts[12].SetActive(true);
     }
     public void onButton11Pressed()
     {
-        texts[1].SetActive(false);
+        texts[11].SetActive(false);
         texts[12].SetActive(true);
     }
     public void onButton12Pressed()
     {
         texts[12].SetActive(false);
         texts[13].SetActive(true);
+        texts[14].SetActive(true);
     }
     public void onButton13Pressed()
     {
         texts[13].SetActive(false);
-        texts[14].SetActive(true);
+        texts[14].SetActive(false);
+        texts[15].SetActive(true);
     }
-    
-    
+    public void onButton14Pressed()
+    {
+        texts[13].SetActive(false);
+        texts[14].SetActive(false);
+        texts[15].SetActive(true);
+    }
+    public void onButton15Pressed()
+    {
+        texts[15].SetActive(false);
+        texts[16].SetActive(true);
+    }
     
 }
