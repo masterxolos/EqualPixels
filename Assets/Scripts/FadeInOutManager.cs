63cb70a965628a409037342b804741c9ef0994a3
Erdem Kaya <195040055@stu.istinye.edu.tr>
2022-03-17 17:42:18 +0300

    Erdemin yaptıkları,

---
+++ Assets/Scripts/FadeInOutManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class FadeInOutManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    
+}
