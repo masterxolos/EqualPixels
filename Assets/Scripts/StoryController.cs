c918dad51695b6edac4176ee0e12d5637b544c4f
masterxolos <ozgur-2000-6@hotmail.com>
2022-03-18 00:09:50 +0300

    son değişiklikler

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -10,6 +10,8 @@ public class StoryController : MonoBehaviour
     [SerializeField] private GameObject[] sahneler = new GameObject[15];
     [SerializeField] private GameObject[] pacmanButtons = new GameObject[15];
     [SerializeField] private GameObject[] pacmans = new GameObject[15];
+
+    [SerializeField] private GameObject fade;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +29,18 @@ public class StoryController : MonoBehaviour
         sahneler[0].SetActive(false);
         sahneler[1].SetActive(true);
         texts[1].SetActive(true);
+        onButton1Pressed();
     }
 
     public void onButton1Pressed()
     {
         Debug.Log("asd");
-        texts[1].SetActive(false);
+        
         texts[2].SetActive(true);
     }
     public void onButton2Pressed()
     {
+        texts[1].SetActive(false);
         texts[2].SetActive(false);
         texts[3].SetActive(true);
         sahneler[1].SetActive(false);
@@ -46,6 +50,8 @@ public class StoryController : MonoBehaviour
         pcyaziları[1].SetActive(true);
         //pcyaziları[2].SetActive(true);
         //pcyaziları[3].SetActive(true);
+        
+        
     }
     public void onButton3Pressed()
     {
@@ -56,13 +62,16 @@ public class StoryController : MonoBehaviour
     }
     public void onButton4Pressed()
     {
-        texts[4].SetActive(false);
+        
         texts[5].SetActive(true);
         texts[6].SetActive(true);
         texts[7].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openQuestionPanel", true);
+
     }
     public void onButton5Pressed()
     {
+        texts[4].SetActive(false);
         texts[5].SetActive(false);
         texts[6].SetActive(false);
         texts[7].SetActive(false);
@@ -71,10 +80,12 @@ public class StoryController : MonoBehaviour
         pcyaziları[8].SetActive(true);
         pacmanButtons[0].SetActive(true);
         pacmans[0].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", true);
         
     }
     public void onButton6Pressed()
     {
+        texts[4].SetActive(false);
         texts[5].SetActive(false);
         texts[6].SetActive(false);
         texts[7].SetActive(false);
@@ -83,9 +94,12 @@ public class StoryController : MonoBehaviour
         pcyaziları[8].SetActive(true);
         pacmanButtons[0].SetActive(true);
         pacmans[0].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", true);
+        
     }
     public void onButton7Pressed()
     {
+        texts[4].SetActive(false);
         texts[5].SetActive(false);
         texts[6].SetActive(false);
         texts[7].SetActive(false);
@@ -94,10 +108,13 @@ public class StoryController : MonoBehaviour
         pcyaziları[8].SetActive(true);
         pacmanButtons[0].SetActive(true);
         pacmans[0].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", true);
     }
 
     public void onPacmanButton1Pressed()
     {
+        fade.GetComponent<Animator>().SetBool("openQuestionPanel", false);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", false);
         pcyaziları[8].GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough;
         pcyaziları[5].SetActive(true);
         pacmans[0].SetActive(false);
@@ -106,13 +123,15 @@ public class StoryController : MonoBehaviour
     }
     public void onButton8Pressed()
     {
-        texts[8].SetActive(false);
+        
         texts[9].SetActive(true);
         texts[10].SetActive(true);
         texts[11].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openQuestionPanel", true);
     }
     public void onButton9Pressed()
     {
+        texts[8].SetActive(false);
         pcyaziları[9].SetActive(true);
         pcyaziları[10].SetActive(true);
         texts[9].SetActive(false);
@@ -120,9 +139,12 @@ public class StoryController : MonoBehaviour
         texts[11].SetActive(false);
         pacmanButtons[1].SetActive(true);
         pacmans[1].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", true);
     }
     public void onPacmanButton2Pressed()
     {
+        fade.GetComponent<Animator>().SetBool("openQuestionPanel", false);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", false);
         pcyaziları[10].GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough;
         pcyaziları[11].SetActive(true);
         pacmans[1].SetActive(false);
@@ -131,6 +153,7 @@ public class StoryController : MonoBehaviour
     }
     public void onButton10Pressed()
     {
+        texts[8].SetActive(false);
         pcyaziları[9].SetActive(true);
         pcyaziları[10].SetActive(true);
         texts[9].SetActive(false);
@@ -141,6 +164,7 @@ public class StoryController : MonoBehaviour
     }
     public void onButton11Pressed()
     {
+        texts[8].SetActive(false);  
         pcyaziları[9].SetActive(true);
         pcyaziları[10].SetActive(true);
         texts[9].SetActive(false);
@@ -151,12 +175,14 @@ public class StoryController : MonoBehaviour
     }
     public void onButton12Pressed()
     {
-        texts[12].SetActive(false);
+        
         texts[13].SetActive(true);
         texts[14].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openQuestionPanel", true);
     }
     public void onButton13Pressed()
     {
+        texts[12].SetActive(false);
         pcyaziları[12].SetActive(true);
         pcyaziları[13].SetActive(true);
         texts[13].SetActive(false);
@@ -164,9 +190,11 @@ public class StoryController : MonoBehaviour
         //texts[15].SetActive(true);
         pacmanButtons[2].SetActive(true);
         pacmans[2].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", true);
     }
     public void onButton14Pressed()
     {
+        texts[12].SetActive(false);
         pcyaziları[12].SetActive(true);
         pcyaziları[13].SetActive(true);
         texts[13].SetActive(false);
@@ -174,9 +202,12 @@ public class StoryController : MonoBehaviour
         
         pacmanButtons[2].SetActive(true);
         pacmans[2].SetActive(true);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", true);
     }
     public void onPacmanButton3Pressed()
     {
+        fade.GetComponent<Animator>().SetBool("openQuestionPanel", false);
+        fade.GetComponent<Animator>().SetBool("openPacManPanel", false);
         texts[15].SetActive(true);
         pcyaziları[13].GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough;
         pcyaziları[14].SetActive(true);
